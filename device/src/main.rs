a031af6196ca0aa2feb2d9398866d08d269de9b7
SamuelNoesslboeck <samuel.noesslboeck@gmail.com>
2023-12-21 15:07:50 +0100

    sync

--- device/src/main.rs
+++ device/src/main.rs
@@ -2,16 +2,13 @@ use rumqttc::QoS;
 use core::time::Duration;
 use std::thread;
 
-mod syws;
-use syws::*;
-
 fn main() {
     let mut client = WorkshopPLCClient::new();
 
     thread::spawn(move || loop {
         client.client.publish(format!("/device/{}", client.id), QoS::AtLeastOnce, false, "true").unwrap();
         thread::sleep(Duration::from_millis(5000));
-     });
+    });
     
     // Iterate to poll the eventloop for connection progress
     for (_, notification) in client.connection.iter().enumerate() {
