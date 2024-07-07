97cbed165628497fa15265b8acee009d0d860576
Samuel Nösslböck <samuel.noesslboeck@gmail.com>
2024-07-08 00:16:25 +0200

    new index script

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,6 @@
 const MAIN_LIGHT_CHAIN_BUTTON = document.getElementById("main_light_chain_button");
 
 MAIN_LIGHT_CHAIN_BUTTON.addEventListener("click", (e) => {
+    console.log("clicked button!");
     workshop.set("ws/light/chain_main", !ws_data["ws/light/chain_main"])
 })
