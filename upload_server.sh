ddfb9598b6683f26f4748b2655e1c23faad56f49
Samuel Nösslböck <samuel.noesslboeck@gmail.com>
2024-07-08 00:03:21 +0200

    new website

--- upload_server.sh
+++ upload_server.sh
@@ -1,2 +1,7 @@
+sudo cp "workshop.conf" "/etc/apache2/sites-available"
+
 # Simple uploading scripts
-sudo cp -r "www" "/var/"
+sudo cp -r "www" "/var/www/workshop"
+
+sudo a2ensite workshop
+sudo systemctl reload apache2
