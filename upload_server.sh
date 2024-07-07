0fb57b1cb993b4c140d7a6c6310f36b43463ae4f
Samuel Nösslböck <samuel.noesslboeck@gmail.com>
2024-07-08 00:38:04 +0200

    permissions

--- upload_server.sh
+++ upload_server.sh
@@ -3,6 +3,9 @@ sudo cp "workshop.conf" "/etc/apache2/sites-available"
 # Simple uploading scripts
 sudo cp -a "www/." "/var/www/workshop"
 
+sudo chgrp -R www-data /var/www
+sudo chmod -R g+w /var/www
+
 sudo a2ensite workshop
 sudo systemctl reload apache2
 sudo service apache2 restart
