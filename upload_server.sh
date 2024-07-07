3c9860024a5f64b62f14f355427f562c39f667eb
Samuel Nösslböck <samuel.noesslboeck@gmail.com>
2024-07-08 00:11:22 +0200

    improved copy

--- upload_server.sh
+++ upload_server.sh
@@ -1,7 +1,7 @@
 sudo cp "workshop.conf" "/etc/apache2/sites-available"
 
 # Simple uploading scripts
-sudo cp -r "www" "/var/www/workshop"
+sudo cp -a "www/." "/var/www/workshop"
 
 sudo a2ensite workshop
 sudo systemctl reload apache2
